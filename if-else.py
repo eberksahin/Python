6fbe6b89dbd5ff8a21f404cc46c869c2eec94f76
Berk Sahin <eberksahin@gmail.com>
2023-03-24 01:11:46 +0300

    added

--- if-else.py
+++ if-else.py
@@ -17,3 +17,40 @@ else:
     print("sayi1 sayi2'den kucuktur.")
 
 print("İf bloğunun disi")
+
+# 2. kullanım modeli
+a = 36
+b = 36
+print("A") if a > b else print("=") if a == b else print("B")
+
+# And
+
+a = 200
+b = 35
+c = 500
+if a > b and c > a:
+  print("Her iki koşul da Doğru.")
+
+# Or
+
+a = 200
+b = 35
+c = 500
+if a > b or a > c:
+  print("Koşullardan en az biri Doğru")
+
+# Not
+
+a = 35
+b = 200
+if not a > b:
+  print("a, b'den büyük DEĞİLDİR")
+
+# pass(Geçiş Bildirimi)
+
+#ififadeler boş olamaz, ancak herhangi bir nedenle içeriği olmayan bir ifadeniz varsa , hata almamak için ifadeyi if girin .
+a = 35
+b = 200
+
+if b > a:
+  pass
