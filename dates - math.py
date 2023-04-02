52a6dda2c3a69eddb41112a825b033cc8b3db923
Berk Sahin <eberksahin@gmail.com>
2023-04-02 06:49:36 +0300

    dates-math updated, math added.

--- dates - math.py
+++ dates - math.py
@@ -1,6 +1,6 @@
 import datetime
 
-#Dates => Tarih
+## Dates => Tarih
 
 x = datetime.datetime.now()
 
@@ -18,3 +18,53 @@ print(x)
 x = datetime.datetime(2018, 6, 1)
 
 print(x.strftime("%B"))
+
+
+## Math
+
+# min() - max() => En yüksek değer ve ya en düşük değeri bulmak için;
+
+x = min(10, 15, 30)
+y = max(10, 15, 30)
+
+print(x)
+print(y)
+
+#abs() => Belirtilen sayının mutlak(pozitif) değerini döndürür.
+
+x = abs(-4.30)
+
+print(x)
+
+#pow()
+
+# x'in değerini y'nin (x üssü y) gücüne döndürür.
+
+x = pow(4, 3)
+
+print(x)
+
+# Math Module => Matematik modülü
+
+import math
+
+# math.sqrt() => Bir sayının karekökünü döndürür.
+
+x = math.sqrt(81)
+
+print(x)
+
+# math.ceil() => Bir sayıyı en yakın tam sayıya yukarı yuvarlar.
+# math.floor() => Bir sayıyı en yakın tam sayıya aşağı yuvarlar.
+
+x = math.ceil(1.8)
+y = math.floor(1.8)
+
+print(x) 
+print(y) 
+
+# math.pi => pi değerini (3.14...) döndürür.
+
+x = math.pi
+
+print(x)
